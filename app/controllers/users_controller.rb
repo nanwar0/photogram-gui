8487d3ac1fb5d3d14d663c29ae49a722a38f59b5
nanwar0 <nina.anwar@chicagobooth.edu>
2024-07-19 01:22:23 +0000

    user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   def list
+    @list_of_users = User.all.order({ :username => :asc })
     render({:template => "users/user_list"})
   end
 
   def show
     @username = params.fetch("username")
+    @user = User.where({:username => @username}).at(0)
     render({:template => "users/show"})
   end
 end
